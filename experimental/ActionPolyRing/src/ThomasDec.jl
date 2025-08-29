5509f89ae76bd16dbd0655d78f0789bb96551b6a
Tobias Braun <tobias.braun2@rwth-aachen.de>
2025-08-29 15:56:46 +0200

    Reduce setup

--- experimental/ActionPolyRing/src/ThomasDec.jl
+++ experimental/ActionPolyRing/src/ThomasDec.jl
@@ -9,29 +9,29 @@
 
 Return the resultant of `f` and `g` regarded as univariate polynomial in the jet variable `v`. The jet variable can also be specified its jet index or its position in `gens(parent(f))`.
 """
-function resultant(apre1::ActionPolyRingElem{T}, apre2::ActionPolyRingElem{T}, var::ActionPolyRingElem{T}) where {T}
-  check_parent(apre1, apre2) && check_parent(apre1, var)
+function resultant(r1::ActionPolyRingElem, r2::ActionPolyRingElem, var::ActionPolyRingElem)
+  check_parent(r1, r2) && check_parent(r1, var)
   @req is_gen(var) "Not a variable"
-  return resultant(apre1, apre2, __vtj(parent(var))[var])
+  return resultant(r1, r2, __vtj(parent(var))[var])
 end
 
-function resultant(apre1::ActionPolyRingElem{T}, apre2::ActionPolyRingElem{T}, i::Int, idx::Vector{Int}) where {T}
-  check_parent(apre1, apre2)
-  apr = parent(apre1)
-  @req __is_valid_jet(apr, i, idx) "Invalid jet variable"
-  jtv = __jtv(apr)
+function resultant(r1::ActionPolyRingElem, r2::ActionPolyRingElem, i::Int, idx::Vector{Int})
+  check_parent(r1, r2)
+  S = parent(r1)
+  @req __is_valid_jet(S, i, idx) "Invalid jet variable"
+  jtv = __jtv(S)
   if haskey(jtv, (i, idx))
-    return apr(resultant(data(apre1), data(apre2), data(jtv[(i, idx)])))
+    return S(resultant(data(r1), data(r2), data(jtv[(i, idx)])))
   end
-  if is_zero(apre1) || is_zero(apre2)
-    return zero(apr)
+  if is_zero(r1) || is_zero(r2)
+    return zero(S)
   end
-  return one(apr)
+  return one(S)
 end
 
-resultant(apre1::ActionPolyRingElem{T}, apre2::ActionPolyRingElem{T}, jet_idx::Tuple{Int,Vector{Int}}) where {T} = resultant(apre1, apre2, jet_idx...)
+resultant(r1::ActionPolyRingElem, r2::ActionPolyRingElem, jet_idx::Tuple{Int,Vector{Int}}) = resultant(r1, r2, jet_idx...)
 
-resultant(apre1::ActionPolyRingElem{T}, apre2::ActionPolyRingElem{T}, i::Int) where {T} = resultant(apre1, apre2, gen(parent(apre1), i))
+resultant(r1::ActionPolyRingElem, r2::ActionPolyRingElem, i::Int) = resultant(r1, r2, gen(parent(r1), i))
 
 @doc raw"""
     discriminant(p::ActionPolyRingElem)
@@ -43,5 +43,88 @@ function discriminant(p::ActionPolyRingElem)
   d = degree(p, ld)
   resul = resultant(p, derivative(p, ld), ld)
   return (-1)^(divexact(d * (d - 1), 2)) * divexact(resul, initial(p))
+end
+
+###############################################################################
+#
+#  Discriminant and its required methods
+#
+###############################################################################
+
+function univariate_coefficients(r::ActionPolyRingElem, var::ActionPolyRingElem)
+  check_parent(r, var)
+  @req is_gen(var) "Not a variable"
+  return univariate_coefficients(r, __vtj(parent(var))[var])
+end
+  
+function univariate_coefficients(r::ActionPolyRingElem, i, idx::Vector{Int})
+  d = degree(r, i, idx)
+  if d == 0
+    return [r]
+  end
+  S = parent(r)
+  jtv = __jtv(S)
+  var = jtv[(i, idx)]
+  v_idx = var_index(var)
+  return
+end
+
+univariate_coefficients(r::ActionPolyRingElem, jet_idx::Tuple{Int, Vector{Int}}) = univariate_coefficients(r, jet_idx...)
+
+univariate_coefficients(r::ActionPolyRingElem, i::Int) = univariate_coefficients(r, gen(parent(r), i))
+
+function initial(apre::ActionPolyRingElem)
+  if is_constant(apre)
+    return apre
+  end
+  res = parent(apre)()
+  ld = leader(apre)
+  ld_ind = var_index(ld)
+  d = degree(apre, ld_ind)
+  for (t,e) in zip(terms(apre), exponents(apre))
+    if e[ld_ind] < d
+      break
+    end
+    res += remove(t, ld)[2] 
+  end
+  return res
+end
+
+###############################################################################
+#
+#  Pseudo reduction
+#
+###############################################################################
+
+function is_reduced(r::ActionPolyRingElem, g::ActionPolyRingElem)
+  check_parent(r, g)
+  @req !is_constant(g) "Cannot reduce with respect to a constant polynomial"
+  if is_constant(r)
+    return true
+  end
+  ld = leader(g)
+  return degree(r, ld) < degree(g, ld)
+end
+
+function is_reduced(r::ActionPolyRingElem, G::Vector{ActionPolyRingElem})
+  for g in G
+    is_reduced(r, g) || return false
+  end
+  for c in univariate_coefficients(r)
+    for g in G
+      is_reduced(c, g) || return false
+    end
+  end
+  return true
+end
+
+function reduce(r::ActionPolyRingElem, G::Vector{ActionPolyRingElem})
+  all(g -> check_parent(r,g), G) 
+  res = r
+  b = one(dpr)
+  if !is_constant(res)
+    
+  end
+  return (res, b)
 end
 
